d5e018c14280f1aa122452e3406121156f3b4e5a
hmh <215795583@qq.com>
2015-05-06 14:23:21 +0800

    为Vocabulary应用提供接口

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ node src\server.js
 ```
 
 访问
-http://localhost:8080/api?q={word}
-or
-http://localhost:8080/api?dict={word}
+http://localhost:8080/api/v?q={word}
+
+返回结果形如：
+
+```
+{
+    "Angular": {
+        "word": "Angular",
+        "related": "Angular JS",
+        "description": "利用数据绑定、依赖注入等技术构建webapp的框架。",
+        "mp3": "http://res.iciba.com/resource/amp3/1/0/d1/8b/d18b8624a0f5f721da7b82365fc562dd.mp3"
+    }
+}
+```
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,29 +2,84 @@ var APIKEY = 'C231706B1BCAAE8D3CEB0E70B5AF138A',
     Iciba = require('../api/iciba'),
     iciba = new Iciba(APIKEY),
     express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    path = require('path'),
+    fs = require('fs');
 
-router.get('/api', function(req, res) {
+var root = path.join(__dirname, '../../');
+var dataDir = path.join(root, 'data');
+fs.exists(dataDir, function (exists) {
+    if (!exists) {
+        fs.mkdir(dataDir);
+    }
+});
+
+var userVocabulary;
+var file = path.join(dataDir, 'vocabulary.json');
+fs.readFile(file, {encoding: 'utf-8', flag: 'a+'}, function (err, data) {
+    if (err) {
+        throw err;
+    }
+    if (data) {
+        userVocabulary = JSON.parse(data);
+    }else{
+        userVocabulary = {};
+    }
+});
+
+router.get('/api', function (req, res) {
     var isDict = !!req.query.dict;
     var word = req.query.q || req.query.dict || '',
         type = req.query.t || 'auto';//auto,en-zh,zh-en...
 
-    var reqHandler = (function(res){
-        return function (err, result){
-            if(err){
+    var reqHandler = (function (res) {
+        return function (err, result) {
+            if (err) {
                 //console.warn(err);
-            }else{
+            } else {
                 //console.log(result);
                 res.send(result);
             }
         };
     })(res);
 
-    if(word){
-        if(isDict){
+    if (word) {
+        if (isDict) {
             iciba.dict(word, reqHandler);
-        }else{
+        } else {
             iciba.query(word, type, reqHandler);
+        }
+    }
+});
+
+
+router.get('/api/v', function (req, res) {
+    var word = req.query.q || '',
+        type = req.query.t || 'auto';//auto,en-zh,zh-en...
+
+    if (word) {
+        if(userVocabulary[word]){
+            res.send(userVocabulary[word]);
+        }else{
+            iciba.query(word, type, function (err, result) {
+                if (err) {
+                    //console.warn(err);
+                } else {
+                    //console.log(result);
+                    userVocabulary[result.word] = {
+                        word: result.word,
+                        related: '',
+                        description: '',
+                        mp3: result.spells[1].mp3
+                    }
+                    fs.writeFile(file, JSON.stringify(userVocabulary), {encoding: 'utf-8', flag: 'w+'}, function (err) {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                    res.send(userVocabulary[result.word]);
+                }
+            });
         }
     }
 });
