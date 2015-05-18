2aaf19183c8c94c25f03f822c9b00e870e6c8868
hmh <215795583@qq.com>
2015-05-18 11:23:26 +0800

    修改dict2 api，防止被屏蔽，增加get api，按顺序尝试使用query、dict和dict2去获取

--- src/api/iciba.js
+++ src/api/iciba.js
@@ -99,57 +99,81 @@ Iciba.prototype.dict = function (word, fn) {
 };
 
 Iciba.prototype.dict2 = function(word, fn){
-    var url = "http://www.iciba.com/" + word;
-    return request.get(url, function (err, res, data) {
-        if (err) {
-            return fn('API request fail');
-        }
-        var result = {
-            word: word,
-            spells: [],
-            translations: [],
-            examples: []
+    var url = "http://www.iciba.com/";
+    return request(url, function(error, response) {//需要先访问主页获取cookie后查询才不会被屏蔽
+        var options = {
+            url: url + word,
+            headers: {
+                'set-cookie':response.headers['set-cookie']
+            }
         };
-        var $ = cheerio.load(data, {
-            ignoreWhitespace: true,
-            xmlMode: true
-        });
-        //console.log($.html());
-        var dictbar = $('#dict_main .dictbar');
-        var i,len;
-        var eg,spell,linkClickFn,match,pos,sent,example;
-        //音标
-        for (eg = dictbar.find('.dict_title .prons .eg'), i = 0, len = eg.length; i < len; i++) {
-            spell = eg.eq(i);
-            linkClickFn = spell.find('a').attr('onclick');
-            match = /'(.*?mp3)'/g.exec(linkClickFn);
-            if (match) {
-                result.spells.push({
-                    text: spell.find('.fl').text(),
-                    mp3: match[1]
+        request(options, function (err, res, data) {
+            if (err) {
+                return fn('API request fail');
+            }
+            var result = {
+                word: word,
+                spells: [],
+                translations: [],
+                examples: []
+            };
+            var $ = cheerio.load(data, {
+                ignoreWhitespace: true,
+                xmlMode: true
+            });
+            //console.log($.html());
+            var dictbar = $('#dict_main .dictbar');
+            var i, len;
+            var eg, spell, linkClickFn, match, pos, sent, example;
+            //音标
+            for (eg = dictbar.find('.dict_title .prons .eg'), i = 0, len = eg.length; i < len; i++) {
+                spell = eg.eq(i);
+                linkClickFn = spell.find('a').attr('onclick');
+                match = /'(.*?mp3)'/g.exec(linkClickFn);
+                if (match) {
+                    result.spells.push({
+                        text: spell.find('.fl').text(),
+                        mp3: match[1]
+                    });
+                }
+            }
+
+            //翻译
+            for (pos = dictbar.find('.group_prons .group_pos'), i = 0, len = pos.length; i < len; i++) {
+                result.translations.push({
+                    type: pos.eq(i).find('.fl').text(),
+                    desc: pos.eq(i).find('.label_list').text().trim()
                 });
             }
-        }
 
-        //翻译
-        for (pos = dictbar.find('.group_prons .group_pos'), i = 0, len = pos.length; i < len; i++) {
-            result.translations.push({
-                type: pos.eq(i).find('.fl').text(),
-                desc: pos.eq(i).find('.label_list').text().trim()
-            });
-        }
+            //例句
+            for (sent = dictbar.siblings('.simple').find('.tab_content1 dl'), i = 0, len = sent.length; i < len; i++) {
+                example = sent.eq(i);
+                result.examples.push({
+                    en: example.find('dt').text().replace(/^\d\./, '').trim(),
+                    cn: example.find('dd').text().trim()
+                });
+            }
+            //console.log(result);
+            return fn(null, result);
+        });
+    })
+};
 
-        //例句
-        for (sent = dictbar.siblings('.simple').find('.tab_content1 dl'), i = 0, len = sent.length; i < len; i++) {
-            example = sent.eq(i);
-            result.examples.push({
-                en: example.find('dt').text().replace(/^\d\./,'').trim(),
-                cn: example.find('dd').text().trim()
-            });
+Iciba.prototype.get = function(word, fn){
+    var apis = ['dict2','dict'];
+    var _this = this;
+    _this.query(word,null,function(err, res){
+        if(err || !res.spells.length){
+            if(apis.length){
+                _this[apis.pop()](word,arguments.callee);
+            }else{
+                fn(null,res);
+            }
+        }else{
+            fn(null,res);
         }
-        //console.log(result);
-        return fn(null, result);
     });
-}
+};
 
 module.exports = Iciba;
